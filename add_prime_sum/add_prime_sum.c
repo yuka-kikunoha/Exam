fa5e06f9514790604c372766048751c37ab9033c
yuka-kikunoha <kyuka@c4r2s17.42tokyo.jp>
2025-11-29 17:41:10 +0900

    hello

---
+++ add_prime_sum/add_prime_sum.c
@@ -0,0 +1,60 @@
+#include <unistd.h>	
+
+int	ft_atoi(char *s)
+{
+	int	res;
+
+	res = 0;
+	while (*s)
+		res = res * 10 + *s++ - 48;
+	return (res);
+}
+
+int	is_prime(int num)
+{
+	int i;
+
+	i = 2;
+	if (num <= 1)
+		return (0);
+	while (i * i <= num)
+	{
+		if (num % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	put_nbr(int n)
+{
+	char	digit;
+
+	if (n >= 10)
+		put_nbr(n / 10);
+	digit = n % 10 + '0';
+	write(1, &digit, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	nbr;
+	int	sum;
+
+	if (ac == 2)
+	{
+		nbr = ft_atoi(av[1]);
+		sum = 0;
+		while (nbr > 0)
+		{
+			if (is_prime(nbr))
+				sum += nbr;
+			nbr--;
+		}
+		put_nbr(sum);
+	}
+	if (ac != 2)
+		put_nbr(0);
+	write(1, "\n", 1);
+	return (0);
+}
--- ft_atoi_base/ft_atoi_base.c
+++ ft_atoi_base/ft_atoi_base.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 char	to_lower(char c)
 {
@@ -21,4 +22,46 @@ int	get_digit(char c, int digit_in_base)
 		return (10 + c - 'a');
 	else
 		return (-1);
+}
+
+
+int	ft_atoibase(const char *str, int str_base)
+{
+	int	result;
+	int	sign;
+	int	digit;
+
+	result = 0;
+	sign = 1;
+	if (str == NULL || str_base < 2 || str_base > 16)
+		return (0);
+	if (*str == '-')
+	{
+		sign = -1;
+		++str;
+	}
+	while (*str)
+	{
+		digit = get_digit(to_lower(*str), str_base);
+		if (digit == -1)
+			break ;
+		result = result * str_base + digit;
+		++str;
+	}
+	return (result * sign);
+}
+
+int main(int ac, char *ag[])
+{
+	int	res;
+
+	res = 0;
+	if (ac == 3)
+	{
+		res = ft_atoibase(ag[1], atoi(ag[2]));
+		printf("%d", res);
+	}
+	printf("%s", "\n");
+
+	return (0);
 }
